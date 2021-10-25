3860e716664429a8328b049bc87648898fff8920
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2021-10-25 23:23:10 +0530

    Update t1practical9.cpp

--- t1practical9.cpp
+++ t1practical9.cpp
@@ -1,1 +1,34 @@
+// Task 1 : Practical 9 : To Call a Constructor 
+#include <iostream>
+
+using namespace std;
+
+class ABC
+{
+    private :
+       int a ;
+    public :
+      ABC()
+      {
+          cout<<"Constructor is Called " << endl ;
+      }
+      void setA(int b) // Setter
+      {
+          a = b ; // Setting the Private Variable a Using Setter
+      }
+      int getA()
+      {
+          return a ;
+      }
+};
+int main()
+{
+    ABC obj ; // Creating Object  Constructor will Called When an Object is Created 
+    int num ;
+    cout<<"Enter the Value of Private Variable : " ;
+    cin>>num ;
+    obj.setA(num) ; // Setting the Value of Private Variable of Class Using Setter 
+    cout<<"The Value of Private Variable is : "<<obj.getA() << endl ; // Getting the Value of a using Getter Function 
+    return 0;
+}
 
