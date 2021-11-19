a5b1c6e7c12cd7a56d5cc97d9314edd2476c3290
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2021-11-19 11:57:07 +0530

    Create t5practical9.cpp

---
+++ t5practical9.cpp
@@ -0,0 +1,29 @@
+// Practical 9 
+// Task 5 : To Use Static data Member to count Object 
+#include <iostream>
+using namespace std ;
+
+class ABC
+{
+    public :
+      static int x ;
+      ABC() // Every time object is Created x get updated 
+      {
+          x++ ;
+      }
+      void show()
+      {
+          cout<<x ;
+      }
+};
+
+int ABC::x = 0 ; // Initilisation of Static Variable
+
+int main()
+{
+    cout<<"\t\tAkash Yadav 200111076 "<<endl<<endl ;
+    ABC obja , objb , objc , objd ;
+    cout<<"The Value of X or Objects Created is : " ;
+    objc.show() ; // It will print the value of Static int 
+    return 0 ;
+}
